6783f1e7affafc22dab41b7d94f76b1dd3b72ac0
Rachel Yang <rachel.yang@students.olin.edu>
2017-02-12 21:57:04 -0500

    checks if point lies within triangle or nah

--- Work_in_Progress/triangle_intersect.c
+++ Work_in_Progress/triangle_intersect.c
@@ -84,7 +84,7 @@ vector vecNorm(vector *v){
     return result;
 }
 
-/*Returns the crosproduct u x v for two vectors
+/*Returns the crossproduct u x v for two vectors
 u and v.*/
 //TODO
 vector crossProduct(vector *u, vector *v){
@@ -147,8 +147,44 @@ vector triangleIntersect(triangle *tri, ray *r){
     return result;
 }
 
-bool inTri(triangle *t, vector *p){
+/*Checks to see if point of intersection lies within the
+bounds of the triangle
 
+Returns true or false*/
+bool inTri(triangle *t, vector *p){
+	// define vectors from point to point of triangle
+	// to define triangle boundaries
+	vector v1 = vecSub(&t->p1, &t->p2);
+	vector v2 = vecSub(&t->p2, &t->p3);
+	vector v3 = vecSub(&t->p3, &t->p1);
+
+	vector n = triangleNormal(t);
+
+	vector cv1 = crossProduct(&v1, &n);
+	vector cv2 = crossProduct(&v2, &n);
+	vector cv3 = crossProduct(&v3, &n);
+
+	// define vectors from given point to 
+	// one of the points that make up the triangle
+	vector fplane1 = vecSub(p, &t->p1);
+	vector fplane2 = vecSub(p, &t->p2);
+	vector fplane3 = vecSub(p, &t->p3);
+
+	// determine if given point lies above
+	// or below plane defined by triangle
+	float dv1 = dotProduct(&v1, &fplane1);
+	float dv2 = dotProduct(&v2, &fplane2);
+	float dv3 = dotProduct(&v3, &fplane3);
+
+	// if all three have positive directions
+	// point does lies within inside triangle
+	// and so returns true (which returns 1)
+	if ((dv1 < 0) && (dv2 < 0) && (dv3 < 0)) {
+		return true;
+	}
+	else {
+		return false;
+	}
 }
 
 int main()
@@ -172,7 +208,7 @@ int main()
     r.start.z = 0;
 
     r.dir.x = 6;
-    r.dir.y = 7.25;
+    r.dir.y = 0;
     r.dir.z = 2;
 
     vector cross = crossProduct(&t.p1, &t.p2);
@@ -180,10 +216,12 @@ int main()
     triangle inv = matInv(&t);
 
     vector intersect = triangleIntersect(&t, &r);
+    bool inside = inTri(&t, &intersect);
 
     printf("%f, %f, %f\n", norm.x, norm.y, norm.z); //The modified vR is the cross product
     printf("|%.3f %.3f %.3f|\n|%.3f %.3f %.3f|\n|%.3f %.3f %.3f|\n", inv.p1.x, inv.p1.y, inv.p1.z, inv.p2.x, inv.p2.y, inv.p2.z, inv.p3.x, inv.p3.y, inv.p3.z);
 
     printf("%f\n", det3x3(&t));
     printf("(%f, %f, %f\n)", intersect.x, intersect.y, intersect.z);
+    printf("%d\n", inside);
 }
