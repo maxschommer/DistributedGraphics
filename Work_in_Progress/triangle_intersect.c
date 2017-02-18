107d8319ca3fd6a23ec2182a198b95b1f9a4ae35
maxschommer <maxschommer@gmail.com>
2017-02-18 16:01:42 -0500

    I got diffusion ray tracing workinglsls!

--- Work_in_Progress/triangle_intersect.c
+++ Work_in_Progress/triangle_intersect.c
@@ -3,8 +3,13 @@ a ray and a triangle*/
 
 #include <stdio.h>
 #include <stdbool.h>
+#include <math.h>
+#define WIDTH 800
+#define HEIGHT 500
 /*#include "../main.h"
 */
+
+
 typedef struct{
     float x,y,z;
 }vector;
@@ -15,30 +20,33 @@ typedef struct{
     vector p3;
 }triangle;
 
+
 typedef struct{
     vector start;
     vector dir;
     float reflectance;
 }ray;
 
-/*This is a fast inverse square root function.
-I did not write it, and barely comprehend it.*/
-float Q_rsqrt(float number )
+typedef struct
 {
-    long i;
-    float x2, y;
-    const float threehalfs = 1.5F;
+	int FLAG;
+	vector point;
+}hit_tri;
 
-    x2 = number * 0.5F;
-    y  = number;
-    i  = * ( long * ) &y;                       // evil floating point bit level hacking
-    i  = 0x5f3759df - ( i >> 1 );               // what the flux? 
-    y  = * ( float * ) &i;
-    y  = y * ( threehalfs - ( x2 * y * y ) );   // 1st iteration
-//  y  = y * ( threehalfs - ( x2 * y * y ) );   // 2nd iteration, this can be removed
+typedef struct
+{
+	vector point;
+	float diff_int; //Diffuse Intensity
+	float spec_int; //Specular Intensity
+}point_light;
 
-    return y;
-}
+//This is used to test Trace code,
+//which requires a global object. We're
+//using a triangle to be our object.
+triangle tglobal;
+point_light light;
+
+
 
 /*Returns a float that is the vector dot product
 of two vectors*/
@@ -80,7 +88,7 @@ vector vecScale(vector *v, float *d){
 
 /*Returns the unit vector of a vector input.*/
 vector vecNorm(vector *v){
-    float invSqrt = Q_rsqrt((v->x)*(v->x)+(v->y)*(v->y)+(v->z)*(v->z));
+    float invSqrt = 1.0/sqrtf((v->x)*(v->x)+(v->y)*(v->y)+(v->z)*(v->z));
     vector result = {(v->x)*invSqrt, (v->y)*invSqrt, (v->z)*invSqrt};
     return result;
 }
@@ -93,7 +101,18 @@ vector crossProduct(vector *u, vector *v){
     result.x = u->y * v->z - u->z * v->y;
     result.y = u->z * v->x - u->x * v->z;
     result.z = u->x * v->y - u->y * v->x;
+    //printf("from crossproduct: %f\n", result);
     return result;
+}
+
+
+/*This is used to calculate the intensity of
+light of a point source. It is the 
+distance function, and takes two vectors,
+and returns the distance between them*/
+float distance(vector *u, vector *v){
+	vector diff = vecSub(u, v);
+	return sqrt(diff.x * diff.x + diff.y * diff.y + diff.z * diff.z);
 }
 
 /*Finds the normal of a triangle and returns
@@ -136,15 +155,26 @@ triangle matInv(triangle *A){
 }
 
 /*Calculates the point of intersection between the ray 
-and the plane of the triangle*/
-vector triangleIntersect(triangle *tri, ray *r){
+and the plane of the triangle. If the ray is firing
+in the wrong direction, then the FLAG variable
+is set to 0, signaling a lack of intersection
+with the plane of the triangle.*/
+hit_tri planeIntersect(triangle *tri, ray *r){
     /*Formula is (a . n)/(r . n)*/
     vector n = triangleNormal(tri);
     vector a = vecSub(&tri->p1, &r->start);
     float d = dotProduct(&a, &n)/dotProduct(&r->dir, &n);
+    if (d<=0)
+    {
+    	hit_tri no_intersect;
+    	no_intersect.FLAG = 0;
+    	return no_intersect;
+    }
     vector v1 = vecScale(&r->dir, &d);
     vector v2 = r->start;
-    vector result = vecSum(&v1, &v2);
+    hit_tri result;
+    result.point = vecSum(&v1, &v2);
+    result.FLAG = 1;
     return result;
 }
 
@@ -152,7 +182,7 @@ vector triangleIntersect(triangle *tri, ray *r){
 bounds of the triangle
 
 Returns true or false*/
-bool inTri(triangle *t, vector *p){
+int inTri(triangle *t, vector *p){
 	// define vectors from point to point of triangle
 	// to define triangle boundaries
 	vector v1 = vecSub(&t->p1, &t->p2);
@@ -180,27 +210,167 @@ bool inTri(triangle *t, vector *p){
 	// if all three have positive directions
 	// point does lies within inside triangle
 	// and so returns true (which returns 1)
-	if ((dv1 >= 0) && (dv2 >= 0) && (dv3 >= 0)) {
-		return true;
+	if ((dv1 <= 0) && (dv2 <= 0) && (dv3 <= 0)) {
+		return 1;
 	}
 	else {
-		return false;
+		return 0;
 	}
 }
-/*Returns a vector in the direction of the reflected
+
+/*Returns the point of intersection between 
+a ray and the nearest object (just one triangle
+for now). Otherwise, it returns None. 
+The inputed triangle should be removed
+when function is verified to work, and
+the function should act on a global 
+collection of triangles called an object*/
+hit_tri Intersect(ray *r){
+	int inside;
+	hit_tri find_intersect = planeIntersect(&tglobal, r);
+    if ((find_intersect.FLAG) && (inTri(&tglobal,&find_intersect.point))){
+    	find_intersect.FLAG = 1;
+    	return find_intersect;
+    }else{
+    	find_intersect.FLAG = 0;
+    	return find_intersect;
+    }
+
+}
+
+
+/*Returns a ray in the direction of the reflected
 ray given by the normal of the surface and the initial
 ray.*/
-vector reflectedRay(ray *d, vector *n){
+ray reflectedRay(ray *d, vector *n, vector *p){
 	float dn = 2*(dotProduct(&d->dir, n));
 	vector c = vecScale(n, &dn);
-	vector r = vecSub(&d->dir, &c);
-	return r;
+	ray result;
+	result.dir = vecSub(&d->dir, &c);
+	result.start = *p;
+	return result;
 }
+
+/*This function accumulates the light gathered
+from all of the light sources. Currently,
+it just does this for one light source. It needs
+to be able to loop.*/
+float AccLightSource(vector *q){
+	float ia = .1; //This needs to become global
+	float color = ia;
+	//For loop goes here, to do all light sources
+	ray r;
+	r.start = *q;
+	r.dir = vecSub(&light.point, q);
+
+	hit_tri w = Intersect(&r);
+	//This checks if the ray intersects
+	//something before hitting a light source.
+	//Its not reliable, and should be improved
+	//if (w.FLAG == 0)
+	//{
+		float kd = .5; //Diffusion/reflection constant
+		vector Lhat = vecNorm(&r.dir); //Normal of vector from point to 
+								//light source.
+		vector Nhat = triangleNormal(&tglobal);//Normal of triangle
+		float light_intensity = 1 / distance(q, &light.point) * light.diff_int;
+		/*if (w.FLAG)
+		{
+			printf("light_intensity: %f\n", light_intensity);
+		}*/
+		float diff_int = dotProduct(&Lhat, &Nhat) * kd * light_intensity;
+
+
+
+
+
+		color += diff_int;
+		//color += specular intensity;*/
+	//}
+	//End the for loop
+
+	return color;
+}
+
+
+float Trace(ray *r, int depth){
+	float ia; //This needs to become global
+	if (depth >4) //Checks if maximum recursion depth is met
+	{
+		return ia;
+	}
+
+	hit_tri q = Intersect(r);
+	//printf("\nq = %f, %f, %f, %d\n", q.point.x, q.point.y,q.point.z, q.FLAG);
+
+	
+	if (q.FLAG==0){
+		//printf("Target Hit!");
+		return ia;
+	}
+	//This is the color of the pixel in intensity  for one 
+	//color chanel (RGB)
+	float color;
+
+
+
+	color = -AccLightSource(&q.point);
+	//printf("color = :%f\n", color);
+	//Reflection and refraction recursion starts here
+	return color;
+}
+
+
+
+//http://stackoverflow.com/questions/2693631/read-ppm-file-and-store-it-in-an-array-coded-with-c
+void writePPM(const char *filename, unsigned char *myimg, int w, int h)
+{
+    FILE *fp;
+    //open file for output
+    fp = fopen(filename, "wb");
+    if (!fp) {
+         fprintf(stderr, "Unable to open file '%s'\n", filename);
+    }
+
+    //write the header file
+    //image format
+    fprintf(fp, "P6\n");
+
+    //image size
+    fprintf(fp, "%d %d\n",w,h);
+
+    // rgb component depth
+    fprintf(fp, "%d\n",255);
+
+    // pixel data
+    fwrite(myimg, 3, w*h, fp);
+    fclose(fp);
+}
+
 
 
 int main()
 {
+	light.point.x = 2;
+	light.point.y = 0;
+	light.point.z = 4;
+	light.diff_int = 700;
+
+	tglobal.p1.x = 3; //Triangle is parallel to the yz plane, and is isoceles.
+	tglobal.p1.y = 3;
+	tglobal.p1.z = 0;
+
+	tglobal.p2.x = 3;
+	tglobal.p2.y = -3;
+	tglobal.p2.z = 0;
+
+	tglobal.p3.x = 6;
+	tglobal.p3.y = -4;
+	tglobal.p3.z = 5;
+
+
     triangle t;
+
     t.p1.x = 3; //Triangle is parallel to the yz plane, and is isoceles.
     t.p1.y = 3;
     t.p1.z = 0;
@@ -210,7 +380,7 @@ int main()
     t.p2.z = 0;
     
     t.p3.x = 3;
-    t.p3.y = 0;
+    t.p3.y = -4;
     t.p3.z = 6;
 
     ray r;
@@ -222,48 +392,67 @@ int main()
     r.dir.y = 0;
     r.dir.z = 0;
 
-    /*vector cross = crossProduct(&t.p1, &t.p2);
-    vector norm = triangleNormal(&t);
-    triangle inv = matInv(&t);
-
-    vector intersect = triangleIntersect(&t, &r);
-    bool inside = inTri(&t, &intersect);
-
-    printf("%f, %f, %f\n", norm.x, norm.y, norm.z); //The modified vR is the cross product
-    printf("|%.3f %.3f %.3f|\n|%.3f %.3f %.3f|\n|%.3f %.3f %.3f|\n", inv.p1.x, inv.p1.y, inv.p1.z, inv.p2.x, inv.p2.y, inv.p2.z, inv.p3.x, inv.p3.y, inv.p3.z);
-
-    printf("%f\n", det3x3(&t));
-    printf("(%f, %f, %f\n)", intersect.x, intersect.y, intersect.z);
-    printf("%d\n", inside);*/
-
-    int y,z;
-    bool inside;
+    int y,z,x;
+    int inside;
 
     for (z=10;z>-11;z--){
         r.dir.z=z;
         printf("\n");
         for (y=-10; y<11;y++){
             r.dir.y = y;
-            vector intersect = triangleIntersect(&t, &r);
-            inside = inTri(&t,&intersect);
-            if (inside){
+            if (Trace(&r, 0)){
                 printf("++");
             }else{
                 printf("--");
             }
         }
     }
+
+//
+
+    unsigned char img[WIDTH * HEIGHT * 3];
+    float yf;
+    float zf;
+    for (z=0;z<HEIGHT;z++){
+    	zf = z;
+        r.dir.z = ((zf-HEIGHT/2)/HEIGHT) * 20;
+        //printf("r.dir.z = %f", r.dir.z);
+        for (y=0;y<WIDTH;y++){
+        	yf = y;
+            r.dir.y = ((yf-WIDTH/2)/WIDTH) * 20;
+            //printf("\nyf: %f", r.dir.y);
+            
+/*            if (Trace(&r, 0)>.11)
+            {
+            	printf("Trace: %f\n", Trace(&r, 0));
+            }*/
+        	
+            img[(WIDTH-y+z*WIDTH)*3 +0] = Trace(&r, 0);
+            img[(WIDTH-y+z*WIDTH)*3 +1] = 0;
+            img[(WIDTH-y+z*WIDTH)*3 +2] = 0;
+
+    
+        }
+    }
+
+    writePPM("image.ppm",img,WIDTH,HEIGHT);
+//
+
+
     r.start.x = 0;
     r.start.y = 0;
     r.start.z = 0;
 
     r.dir.x = 4;
-    r.dir.y = 0;
+    r.dir.y = 1;
     r.dir.z = 0;
+
+    hit_tri intersect = planeIntersect(&t, &r);
     vector norm = triangleNormal(&t);
-    vector Ref = reflectedRay(&r, &norm);
+    ray Ref = reflectedRay(&r, &norm, &intersect.point);
     printf("\nNormal:      \n%f, %f, %f\n", norm.x, norm.y, norm.z);
     printf("Unreflected: \n%f, %f, %f\n", r.dir.x, r.dir.y, r.dir.z);
-    printf("Reflected:   \n%f, %f, %f\n", Ref.x, Ref.y, Ref.z);
+    printf("Reflected Direction:   \n%f, %f, %f\n", Ref.dir.x, Ref.dir.y, Ref.dir.z);
+    printf("Reflected Start:  %f, %f, %f\n", Ref.start.x, Ref.start.y, Ref.start.z);
     return 0;
 }
