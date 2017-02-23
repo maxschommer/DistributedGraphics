4081c8566c48829016bfd460f044c237bf5eb1e5
Jeremy Garcia <jeremiah.garcia@students.olin.edu>
2017-02-23 11:21:56 -0500

    finished stl parser

--- Work_in_Progress/parse_stl.c
+++ Work_in_Progress/parse_stl.c
@@ -3,8 +3,10 @@
 #include <string.h>
 #include <regex.h> 
 
+float numbers[25][3][3];
 
-int Search_in_File(char *fname) {
+//parses a file line by line and adds points to 3d array if line contains the string "vertex"
+int search_for_vertex(char *fname) {
 	FILE *fp;
 	int line_num = 1;
 	int find_result = 0;
@@ -14,45 +16,37 @@ int Search_in_File(char *fname) {
 		return(-1);
 	}
 
-	// float numbers[12][3][3];
-	// int i,j,k;
-
-	// for (i=0;i<12;i++){
-	// 	for (j=0;j<3;j++){
-	// 		for(k=0;k<3;k++){
-
-	// 		}
-	// 	}
-	// }
-
 	int i = 0;
+	int j = 0;
+
 	while(fgets(temp, 512, fp) != NULL) {
 		if((strstr(temp, "vertex")) != NULL) {
-			printf("A match found on line: %d\n", line_num);
-			printf("\n%s\n", temp);
+			//printf("A match found on line: %d\n", line_num);
+			//printf("\n%s\n", temp);
 
-			float numbers[12][3];
 			char g[10];
-			if (sscanf(temp,"%s %f %f %f",g, &numbers[i][0], &numbers[i][1], &numbers[i][2])){
-				printf("%f %f %f\n", numbers[i][0], numbers[i][1], numbers[i][2]);
-				i++;
+			if (sscanf(temp,"%s %f %f %f",g, &numbers[i][j][0], &numbers[i][j][1], &numbers[i][j][2])){
+				//printf("%f %f %f\n", numbers[i][j][0], numbers[i][j][1], numbers[i][j][2]);
+				j++;
+				if (j==3){
+					i++;
+					//printf("\n");
+					j=0;
+				}
 			};
-			//printf("%f %f %f\n", numbers[0], numbers[1], numbers[2]);
 
 			find_result++;
 		}
 		line_num++;
 	}
 
-
 	if(find_result == 0) {
 		printf("\nSorry, couldn't find a match.\n");
 	}
 	
-	//Close the file if still open.
-	if(fp) {
-		fclose(fp);
-	}
+	//Close the file
+	fclose(fp);
+
    	return(0);
 }
 
@@ -63,11 +57,21 @@ int main(int argc, char *argv[]) {
 	//Use system("clear") on Unix/Linux
 	system("clear");
 
-	result = Search_in_File(argv[1]);
+	result = search_for_vertex(argv[1]);
 	if(result == -1) {
 		perror("Error");
 		printf("Error number = %d\n", errno);
 		exit(1);
 	}
+
+	//prints out content of 3d array
+	int i,j;
+	for (i=0;i<12;i++){
+		for(j=0;j<3;j++){
+			printf("%f %f %f\n", numbers[i][j][0], numbers[i][j][1], numbers[i][j][2]);
+		}
+		printf("%d\n",i);
+	}
+
 	return(0);
 }
