83c936955f5ae0208af6d81021dd75ecdeb42e4b
DHZBill <haozheng.du@students.olin.edu>
2017-02-27 00:26:35 -0500

    finish rotationMatrix

--- Work_in_Progress/rotationMatrix.c
+++ Work_in_Progress/rotationMatrix.c
@@ -77,21 +77,48 @@ vector zRotate(vector *v, float gamma){
 	return new_vector;
 }
 
-vector rotate(vector *v, float x, float y, float z){
+vector rotate(vector *v, vector *origin, float x, float y, float z){
 	vector new_vector;
 	vector v1, v2;
-	v1 = xRotate(v, x);
+	v1 = vecSub(v, origin);
+	v1 = xRotate(&v1, x);
 	v2 = yRotate(&v1, y);
 	new_vector = zRotate(&v2, z);
+	new_vector = vecAdd(&new_vector, origin);
 	return new_vector;
 }
 
+vector rotateAboutVector(vector *v, vector *line, float theta){
+	float temp, cosVal, sinVal;
+	vector vRot, tempV0, tempV1, tempV2, tempV3, unit;
+	float rad = PI / 180 * theta;
+	cosVal = (float) cos(rad);
+	sinVal = (float) sin(rad);
+	tempV0 = vecScale(v, &cosVal);
+	unit = vecNorm(line);
+	tempV2 = crossProduct(&unit, v);
+	tempV3 = vecScale(&tempV2, &sinVal);
+	tempV0 = vecAdd(&tempV0, &tempV3);
+	temp = dotProduct(&unit, v) * (1-cos(rad)); 
+	tempV1.x = unit.x * temp;
+	tempV1.y = unit.y * temp;
+	tempV1.z = unit.z * temp;
+	vRot = vecAdd(&tempV0, &tempV1);
+	return vRot; 
+}
+
 int main(){
-	vector v, u;
+	vector v, origin, u, a;
 	v.x = 3;
 	v.y = 4;
 	v.z = 5;
 
-	u = rotate(&v, 0, 45, 0);
+	origin.x = 0;
+	origin.y = 0;
+	origin.z = 0;
+
+	u = rotate(&v, &origin, 0, 45, 0);
+	a = rotateAboutVector(&v, &v, 90);
 	printf("the new vector is (%f, %f, %f)\n", u.x, u.y, u.z);
+	printf("the new vector is (%f, %f, %f)\n", a.x, a.y, a.z);
 }
