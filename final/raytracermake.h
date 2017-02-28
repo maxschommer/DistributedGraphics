13a80d6e466de97e6d38b8cfd19e4394db843d80
DHZBill <haozheng.du@students.olin.edu>
2017-02-27 20:49:50 -0500

    Commented raytracermake.h

--- final/raytracermake.h
+++ final/raytracermake.h
@@ -1,33 +1,45 @@
+/*
+ * Header file defining structs used in the ray tracer.
+ */
+
+// Defines a 3D vector
 typedef struct{
-    float x,y,z;
+    float x,y,z; // Positions in x, y, z axis
 }vector;
 
+// Defines a triangle
 typedef struct{
+    // Three vectors that define the position of the triangle
     vector p1;
     vector p2;
     vector p3;
-    vector normal;
-    float ks;
-    float kd;
-    float ka;
-    float gamma;
+
+    vector normal; // The normal of the triangle    
+    float ks; // Specular reflection constant
+    float kd; // Diffuse reflection constant
+    float ka; // Ambient reflection constant
+    float gamma; // Shininess constant
 }triangle;
 
+// Defines a ray of light
 typedef struct{
-    vector start;
-    vector dir;
-    float reflectance;
+    vector start; // Position of the light source
+    vector dir; // Direction of the light source
+    float reflectance; // Reflectance
 }ray;
 
+// Defines the intersection between a vector and a triangle
 typedef struct
 {
-	int FLAG;
-    int index;
-    vector normal;
-	vector point;
+	int FLAG; // 1 if the light hits the triangle, 0 otherwise
+    int index; // Index of the triangle 
+    vector normal; // The normal to the triangle that passes through
+                   // the point of intersection
+	vector point;  // Point of intersection
 
 }hit_tri;
 
+// Defines a point of light
 typedef struct
 {
 	vector point;
